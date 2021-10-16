01886e054249cc502e8042cf65b830808bcf9b26
luk-kop <luk.kopicki@gmail.com>
2021-10-16 07:00:22 +0200

    Updated constants.py file (ShipType class).

--- constants.py
+++ constants.py
@@ -3,7 +3,7 @@ from enum import IntEnum
 
 class MmsiCountry(IntEnum):
     """
-    Maritime Identification Digits (MID). Only selected countries are listed
+    Maritime Identification Digits (MID). Only selected countries are listed.
     For all codes see: https://www.itu.int/en/ITU-R/terrestrial/fmd/Pages/mid.aspx
     """
     Belgium = 205
@@ -41,7 +41,7 @@ class MmsiCountry(IntEnum):
 
 class NavigationStatus(IntEnum):
     """
-    Navigational status fo AIS msg type 1.
+    Navigational status for AIS msg type 1.
     """
     Under_way_using_engine = 0
     At_anchor = 1
@@ -54,6 +54,42 @@ class NavigationStatus(IntEnum):
     Under_way_sailing = 8
     AISSART_active = 14
     Undefined = 15
+
+    @classmethod
+    def has_value(cls, value):
+        values = set(item.value for item in cls)
+        return value in values
+
+
+class ShipType(IntEnum):
+    """
+    Ship type codes for AIS msg type 5. Only selected ship types are listed.
+    For all codes see: https://gpsd.gitlab.io/gpsd/AIVDM.html#_aivdmaivdo_sentence_layer
+    """
+    Not_available = 0
+    # 30's
+    Wing_in_ground = 20
+    Fishing = 30
+    Towing = 31
+    Towing_length_over_200m = 32
+    Dredging_or_underwater_ops = 33
+    Diving_ops = 34
+    Military_ops = 35
+    Sailing = 36
+    Pleasure_craft = 37
+    High_speed_craft = 40
+    Pilot_vessel = 50
+    SAR_vessel = 51
+    Tug = 52
+    Port_tender = 53
+    Anti_pollution_equipment = 54
+    Law_enforcement = 55
+    Medical_transport = 58
+    Non_combat_ship = 59
+    Passenger = 60
+    Cargo = 70
+    Tanker = 80
+    Other_type = 90
 
     @classmethod
     def has_value(cls, value):
